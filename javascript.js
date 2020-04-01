841c185763ccafc6a06c4d32ec8a24e242227e84
Todd Leslie <spotontechnology@gmail.com>
2020-03-31 19:02:40 -0600

    2nd commit

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var questionNumber = 0, quiz, quizScore, question, choice, choices, selectA, selectB, selectC, selectD, correct =0;
 var questions = [
-    ["What year did the Denver Broncos win their last title?","2011", "2013","2015","2016","D"],
+    ["What year did the Denver Browncos win their last title?","2011", "2013","2015","2016","D"],
     ["What is that thing on Trumps head","raccoon","whig","roadkill","unknown","unknown"],
     ["Who is the greatest basketball player of all time?","Lebron James","Michael Jordan","Bill Russell","Larry Bird","B"],
     ["Who is the most underrated inventor of all time?","Tesla","Edison","DiVinci","Bell","A"]
@@ -10,7 +10,8 @@ return document.getElementById(x);
 }
 function askQuestion() {
 quiz = _("quiz");
-if questionNumber >= questions.length) {
+var quizScore=_("quizScore")
+if (questionNumber >= questions.length) {
     quiz.innerHTML = "<h2>You got "+correct+ "of "+questions.length+" questions right</h2>";
     _("quizScore").innerHTML = "Test Complete";
     questionNumber=0;
@@ -18,19 +19,19 @@ if questionNumber >= questions.length) {
     return false;
     
 }
-_("quizScore").innerHTML = "Question "+(questionNumber+1)+"of "+questions; question = questions[questionNumber] [0];
+_("quizScore").innerHTML = "Question "+(questionNumber)+"of "+questions; question = questions[questionNumber] [0];
 selectA = questions[questionNumber][1];
 selectB = questions[questionNumber][2];
 selectC = questions[questionNumber][3];
 selectD = questions[questionNumber][4];
 
-quiz.innerHTML = "<h3>"+question+"</h3>";
-quiz.innerHTML += "<input type='radio' name='choices' value='A'> "+selectA+"<br>";
-quiz.innerHTML += "<input type='radio' name='choices' value='B'> "+selectB+"<br>";
-quiz.innerHTML += "<input type='radio' name='choices' value='C'> "+selectC+"<br>";
-quiz.innerHTML += "<input type='radio' name='choices' value='D'> "+selectD+"<br>";
+quizScore.innerHTML = "<h3>"+question+"</h3>";
+quizScore.innerHTML += "<input type='radio' name='choices' value='A'> "+selectA+"<br>";
+quizScore.innerHTML += "<input type='radio' name='choices' value='B'> "+selectB+"<br>";
+quizScore.innerHTML += "<input type='radio' name='choices' value='C'> "+selectC+"<br>";
+quizScore.innerHTML += "<input type='radio' name='choices' value='D'> "+selectD+"<br>";
 
-quiz.innerHTML+= "<button onclick='checkAnswer()'>Submit</button>";
+//quiz.innerHTML+= "<button onclick='checkAnswer()'>Submit</button>";
 
 }
 function checkAnswer(){
@@ -44,10 +45,10 @@ if(choice == questions[questionNumber] [5]) {
     correct++;
 
 } 
-
-
-}
 questionNumber++;
-askQuestion();
+askQuestion()
+
 }
+_("quiz").innerHTML+= "<button onclick='checkAnswer()'>Submit</button>";
+
 window.addEventListener("load", askQuestion, false);
