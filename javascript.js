b66f41a12de6581fd792bec1d53bb6fd7946f6b7
Todd Leslie <spotontechnology@gmail.com>
2020-03-31 20:49:34 -0600

    3rd comm

--- javascript.js
+++ javascript.js
@@ -48,7 +48,29 @@ if(choice == questions[questionNumber] [5]) {
 questionNumber++;
 askQuestion()
 
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes
+    function tick() {
+         
+        var counter = document.getElementById("counter");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+            if(mins > 1){
+                countdown(mins-1);           
+            }
+        }
+    }
+    tick();
+}
 }
 _("quiz").innerHTML+= "<button onclick='checkAnswer()'>Submit</button>";
+
+
+
 
 window.addEventListener("load", askQuestion, false);
